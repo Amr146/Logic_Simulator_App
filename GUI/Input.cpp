074c1aa49bf5875ef5846b38bb10bee01c25723c
Amr146 <76057767+Amr146@users.noreply.github.com>
2020-12-23 11:37:49 +0200

    Add files via upload

---
+++ GUI/Input.cpp
@@ -0,0 +1,149 @@
+//#include "Input.h"
+#include "Output.h"
+
+Input::Input(window* pW)
+{
+	pWind = pW; //point to the passed window
+}
+
+void Input::GetPointClicked(int &x, int &y)
+{
+	pWind->WaitMouseClick(x, y);	//Wait for mouse click
+}
+
+string Input::GetSrting(Output *pOut)
+{
+	pWind->FlushKeyQueue();
+	string s;
+	char c;
+	do
+	{
+		pWind->WaitKeyPress(c); //wait for key presse
+		if (c == '\015') //return the user-entered string when the user presses "ENTER".
+		{
+			pOut->ClearStatusBar();
+			pWind->FlushMouseQueue();
+			return s;
+		}
+		else if (c == '\010') //delet the last character entered when the user presses "BACKSPACE".
+		{
+			if (s.length() == 0)
+				continue;
+			s.pop_back();
+		}
+		else if (c == '\033') //return an empty string when the user presses "ESCAPE".
+		{
+			pOut->ClearStatusBar();
+			pWind->FlushMouseQueue();
+			return "";
+		}
+		else
+		{
+			s.push_back(c);  //add the pressed character to the string variable.
+		}
+		pOut->PrintMsg(s);   //update the text displayed in the Status bar.
+	} while (true);
+}
+
+//This function reads the position where the user clicks to determine the desired action
+ActionType Input::GetUserAction() const
+{	
+	int x,y;
+	pWind->WaitMouseClick(x, y);	//Get the coordinates of the user click
+
+	if(UI.AppMode == DESIGN )	//application is in design mode
+	{
+		//[1] If user clicks on the Toolbar
+		if ( y >= 0 && y < UI.ToolBarHeight)
+		{	
+			//Check whick Menu item was clicked
+			//==> This assumes that menu items are lined up horizontally <==
+			int ClickedItemOrder = (x / UI.ToolItemWidth);
+			//Divide x coord of the point clicked by the menu item width (int division)
+			//if division result is 0 ==> first item is clicked, if 1 ==> 2nd item and so on
+			if (UI.gatesGroup == 1)
+			{
+				switch (ClickedItemOrder)
+				{
+				case ITM_Buff: return ADD_Buff;
+				case ITM_INV: return ADD_INV;
+				case ITM_AND2: return ADD_AND_GATE_2;
+				case ITM_OR2: return ADD_OR_GATE_2;
+				case ITM_NAND2: return ADD_NAND_GATE_2;
+				case ITM_NOR2: return ADD_NOR_GATE_2;
+				case ITM_NEXT: return NEXT;
+				}
+			}
+			else
+			{
+				switch (ClickedItemOrder + 7)
+				{
+				case ITM_PREVIOUS: return PREVIOUS;
+				case ITM_XOR2: return ADD_XOR_GATE_2;
+				case ITM_XNOR2: return ADD_XNOR_GATE_2;
+				case ITM_AND3: return ADD_AND_GATE_3;
+				case ITM_NOR3: return ADD_NOR_GATE_3;
+				case ITM_XOR3: return ADD_XOR_GATE_3;
+				}
+			}
+				switch (ClickedItemOrder+6)
+				{
+				case ITM_Switch: return ADD_Switch;
+				case ITM_LED: return ADD_LED;
+				case ITM_CONNECTION: return ADD_CONNECTION;
+				case ITM_SIM_MODE: return SIM_MODE;
+				case ITM_EXIT: return EXIT;
+
+				default: return DSN_TOOL;	//A click on empty place in desgin toolbar
+				}
+		}
+	
+		//[2] User clicks on the drawing area
+		if ( y >= UI.ToolBarHeight && y < UI.height - UI.StatusBarHeight)
+		{
+			return SELECT;	//user want to select/unselect a component
+		}
+		
+		//[3] User clicks on the status bar
+		return STATUS_BAR;
+	}
+	else	//Application is in Simulation mode
+	{
+		//[1] If user clicks on the Toolbar
+		if (y >= 0 && y < UI.ToolBarHeight)
+		{
+			//Check whick Menu item was clicked
+			//==> This assumes that menu items are lined up horizontally <==
+			int ClickedItemOrder = (x / UI.ToolItemWidth);
+			//Divide x coord of the point clicked by the menu item width (int division)
+			//if division result is 0 ==> first item is clicked, if 1 ==> 2nd item and so on
+		
+			switch (ClickedItemOrder)
+			{
+			case ITM_SIM: return SIM_MODE;
+			case ITM_DSN_MODE: return DSN_MODE;
+			case ITM_TRUTH: return Create_TruthTable;
+			case ITM_SAVE: return SAVE;
+			case ITM_LOAD: return LOAD;
+			case ITM_DELETE: return DEL;
+			
+			default: return SIM_TOOL;	//A click on empty place in Simulation toolbar
+			}
+		}
+
+		//[2] User clicks on the drawing area
+		if (y >= UI.ToolBarHeight && y < UI.height - UI.StatusBarHeight)
+		{
+			return SELECT;	//user want to select/unselect a component
+		}
+
+		//[3] User clicks on the status bar
+		return STATUS_BAR;
+	}
+
+}
+
+
+Input::~Input()
+{
+}
---
+++ GUI/Output.cpp
@@ -0,0 +1,396 @@
+#include "Output.h"
+
+Output::Output()
+{
+	//Initialize user interface parameters
+
+	UI.AppMode = DESIGN;	//Design Mode is the startup mode
+	UI.gatesGroup = 1;
+
+	//Initilaize interface colors
+	UI.DrawColor = BLACK;
+	UI.SelectColor = BLUE;
+	UI.ConnColor = RED;
+	UI.MsgColor = BLUE;
+	UI.BkGrndColor = WHITE;
+	
+	//Create the drawing window
+	pWind = CreateWind(UI.width, UI.height, UI.wx, UI.wy);	
+	ChangeTitle("Programming Techniques Project");
+
+	CreateDesignToolBar();	//Create the desgin toolbar
+	CreateStatusBar();		//Create Status bar
+
+}
+
+Input* Output::CreateInput() const
+{
+	//Create an Input Object & assign it to the Same Window
+	Input* pIn = new Input(pWind);
+	return pIn;
+}
+
+
+//======================================================================================//
+//								Interface Functions										//
+//======================================================================================//
+
+window* Output::CreateWind(int wd, int h, int x, int y) const
+{
+	return new window(wd, h, x, y);
+}
+//////////////////////////////////////////////////////////////////////////////////
+void Output::ChangeTitle(string Title) const
+{
+	pWind->ChangeTitle(Title);
+}
+//////////////////////////////////////////////////////////////////////////////////
+void Output::CreateStatusBar() const
+{
+	pWind->SetPen(RED,3);
+	pWind->DrawLine(0, UI.height-UI.StatusBarHeight, UI.width, UI.height-UI.StatusBarHeight);
+}
+//////////////////////////////////////////////////////////////////////////////////
+void Output::PrintMsg(string msg) const
+{
+	ClearStatusBar();	//Clear Status bar to print message on it
+	// Set the Message offset from the Status Bar
+	int MsgX = 25;
+	int MsgY = UI.StatusBarHeight - 10;
+
+	// Print the Message
+    pWind->SetFont(20, BOLD | ITALICIZED, BY_NAME, "Arial"); 
+	pWind->SetPen(UI.MsgColor); 
+	pWind->DrawString(MsgX, UI.height - MsgY, msg);
+}
+//////////////////////////////////////////////////////////////////////////////////
+void Output::ClearStatusBar()const
+{
+	// Set the Message offset from the Status Bar
+	int MsgX = 25;
+	int MsgY = UI.StatusBarHeight - 10;
+
+	//Overwrite using bachground color to erase the message
+	pWind->SetPen(UI.BkGrndColor);
+	pWind->SetBrush(UI.BkGrndColor);
+	pWind->DrawRectangle(MsgX, UI.height - MsgY, UI.width, UI.height);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+//Clears the drawing (degin) area
+void Output::ClearDrawingArea() const
+{
+	pWind->SetPen(RED, 1);
+	pWind->SetBrush(WHITE);
+	pWind->DrawRectangle(0, UI.ToolBarHeight, UI.width, UI.height - UI.StatusBarHeight);
+	
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+//Draws the menu (toolbar) in the Design mode
+void Output::CreateDesignToolBar() const
+{
+	UI.AppMode = DESIGN;	//Design Mode
+
+	//You can draw the tool bar icons in any way you want.
+
+	//First prepare List of images for each menu item
+	string MenuItemImages[ITM_DSN_CNT];
+	MenuItemImages[ITM_Buff] = "images\\Menu\\Menu_Buff.jpg";
+	MenuItemImages[ITM_INV] = "images\\Menu\\Menu_INV.jpg";
+	MenuItemImages[ITM_AND2] = "images\\Menu\\Menu_AND2.jpg";
+	MenuItemImages[ITM_OR2]  = "images\\Menu\\Menu_OR2.jpg";
+	MenuItemImages[ITM_NAND2]  = "images\\Menu\\Menu_NAND2.jpg";
+	MenuItemImages[ITM_NOR2]  = "images\\Menu\\Menu_NOR2.jpg";
+	MenuItemImages[ITM_NEXT] = "images\\Menu\\Menu_Next.jpg";
+	MenuItemImages[ITM_PREVIOUS] = "images\\Menu\\Menu_Previous.jpg";
+	MenuItemImages[ITM_XOR2]  = "images\\Menu\\Menu_XOR2.jpg";
+	MenuItemImages[ITM_XNOR2]  = "images\\Menu\\Menu_XNOR2.jpg";
+	MenuItemImages[ITM_AND3]  = "images\\Menu\\Menu_AND3.jpg";
+	MenuItemImages[ITM_NOR3]  = "images\\Menu\\Menu_NOR3.jpg";
+	MenuItemImages[ITM_XOR3]  = "images\\Menu\\Menu_XOR3.jpg";
+	MenuItemImages[ITM_Switch]  = "images\\Menu\\Menu_Switch.jpg";
+	MenuItemImages[ITM_LED]  = "images\\Menu\\Menu_LED.jpg";
+	MenuItemImages[ITM_CONNECTION]  = "images\\Menu\\Menu_CONNECTION.jpg";
+	MenuItemImages[ITM_SIM_MODE] = "images\\Menu\\Menu_SIM_MODE.jpg";
+	MenuItemImages[ITM_EXIT] = "images\\Menu\\Menu_Exit.jpg";
+
+	//TODO: Prepare image for each menu item and add it to the list
+
+	
+
+	//Draw menu item one image at a time
+	if (UI.gatesGroup == 1)
+	{
+		for (int i = 0; i < 6; i++)
+			pWind->DrawImage(MenuItemImages[i], i * UI.ToolItemWidth, 0, UI.ToolItemWidth, UI.ToolBarHeight);
+		pWind->SetPen(RED, 3);
+		pWind->DrawLine(UI.ToolItemWidth * 6, UI.ToolBarHeight, UI.ToolItemWidth * 6, 0);
+		pWind->DrawImage(MenuItemImages[6], 6 * UI.ToolItemWidth, 0, UI.ToolItemWidth, UI.ToolBarHeight);
+	}
+	else
+	{
+		pWind->DrawImage(MenuItemImages[7],0, 0, UI.ToolItemWidth, UI.ToolBarHeight);
+		pWind->SetPen(RED, 3);
+		pWind->DrawLine(UI.ToolItemWidth, UI.ToolBarHeight, UI.ToolItemWidth, 0);
+		for (int i = 1; i < 6; i++)
+			pWind->DrawImage(MenuItemImages[i + 7], i * UI.ToolItemWidth, 0, UI.ToolItemWidth, UI.ToolBarHeight);
+		pWind->SetPen(WHITE, 1);
+		pWind->SetBrush(WHITE);
+		pWind->DrawRectangle(6 * UI.ToolItemWidth, 0, 7 * UI.ToolItemWidth, UI.ToolBarHeight);
+	}
+	pWind->SetPen(RED, 6);
+	pWind->DrawLine(UI.ToolItemWidth * 7, UI.ToolBarHeight, UI.ToolItemWidth * 7, 0);
+	for (int i = 13; i < ITM_DSN_CNT; i++)
+		pWind->DrawImage(MenuItemImages[i], (i-6) * UI.ToolItemWidth, 0, UI.ToolItemWidth, UI.ToolBarHeight);
+
+
+	//Draw a line under the toolbar
+	pWind->SetPen(RED,3);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);	
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//Draws the menu (toolbar) in the simulation mode
+void Output::CreateSimulationToolBar() const
+{
+	UI.AppMode = SIMULATION;	//Simulation Mode
+	string MenuItemImages[ITM_SIM_CNT];
+	MenuItemImages[ITM_SIM] = "images\\Menu\\Menu_SIM_MODE.jpg";
+	MenuItemImages[ITM_DSN_MODE] = "images\\Menu\\Menu_DSN_MODE.jpg";
+	MenuItemImages[ITM_TRUTH] = "images\\Menu\\Menu_TRUTH.jpg";
+	MenuItemImages[ITM_SAVE] = "images\\Menu\\Menu_SAVE.jpg";
+	MenuItemImages[ITM_LOAD] = "images\\Menu\\Menu_LOAD.jpg";
+	MenuItemImages[ITM_DELETE] = "images\\Menu\\Menu_DELETE.jpg";
+
+
+	for(int i=0; i<ITM_SIM_CNT; i++)
+		pWind->DrawImage(MenuItemImages[i],i*UI.ToolItemWidth,0,UI.ToolItemWidth, UI.ToolBarHeight);
+
+	pWind->SetPen(WHITE, 1);
+	pWind->SetBrush(WHITE);
+	pWind->DrawRectangle(ITM_SIM_CNT * UI.ToolItemWidth, 0, UI.width, UI.ToolBarHeight);
+
+	//Draw a line under the toolbar
+	pWind->SetPen(RED,3);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);
+
+	//TODO: Write code to draw the simualtion toolbar (similar to that of design toolbar drawing)
+	//edited
+
+}
+
+//======================================================================================//
+//								Components Drawing Functions							//
+//======================================================================================//
+
+void Output::DrawAND2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\and.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\and.jpg";
+
+	//Draw AND2 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+//TODO: Add similar functions to draw all components	===================================>>>>>>>>>>>>		DONE
+
+void Output::DrawOR2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\or.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\or.jpg";
+
+	//Draw OR2 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+void Output::DrawNOT(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\not.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\not.jpg";
+
+	//Draw NOT Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+void Output::DrawNAND2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\nand.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\nand.jpg";
+
+	//Draw NAND2 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+void Output::DrawNOR2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\nor.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\nor.jpg";
+
+	//Draw OR2 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+
+void Output::DrawXOR2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\xor.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\xor.jpg";
+
+	//Draw XOR2 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+
+void Output::DrawXNOR2(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\xnor.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\xnor.jpg";
+
+	//Draw XNOR2 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+
+void Output::DrawBUFFER(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\buffer.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\buffer.jpg";
+
+	//Draw BUFFER Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+
+void Output::DrawAND3(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\and3.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\and3.jpg";
+
+	//Draw AND3 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+
+void Output::DrawNOR3(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\nor3.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\nor3.jpg";
+
+	//Draw NOR3 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+
+void Output::DrawXOR3(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\xor3.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\xor3.jpg";
+
+	//Draw XOR3 Gate at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+
+void Output::DrawSWITCH(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\switch_off.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\switch_off.jpg";
+
+	//Draw SWITCH at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+
+void Output::DrawLED(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	string GateImage;
+	if(selected)	//use image in the highlighted case
+		GateImage="Images\\Gates\\Hilighted\\led_off.jpg";
+	else  
+		GateImage = "Images\\Gates\\Normal\\led_off.jpg";
+
+	//Draw LED at Gfx_Info (1st corner)
+	//Set the Image Width & Height by AND2 Image Parameter in UI_Info
+	pWind->DrawImage(GateImage, r_GfxInfo.x1, r_GfxInfo.y1, UI.AND2_Width, UI.AND2_Height);
+}
+
+
+void Output::DrawConnection(GraphicsInfo r_GfxInfo, bool selected) const
+{
+	//TODO: Add code to draw connection		======================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>		DONE
+
+	if(selected)	//use connection in the highlighted case
+		pWind->SetPen(UI.SelectColor,3);
+	else  
+		pWind->SetPen(UI.DrawColor,3);
+
+	pWind->DrawLine(r_GfxInfo.x1, r_GfxInfo.y1, r_GfxInfo.x2, r_GfxInfo.y2);	
+}
+
+void Output::PrintLabel(GraphicsInfo r_GfxInfo, string lbl) const
+{
+	// Print the label
+	pWind->SetFont(20, BOLD | ITALICIZED, BY_NAME, "Arial");
+	pWind->SetPen(UI.MsgColor);
+
+	int lblWidth, lblHeight;
+	pWind->GetStringSize(lblWidth, lblHeight, lbl);
+	while(r_GfxInfo.x1 < 0 || r_GfxInfo.x1 > UI.width - (lblWidth+3*lbl.length()) || r_GfxInfo.y1 < UI.ToolBarHeight || r_GfxInfo.y1 > UI.height - UI.StatusBarHeight - lblHeight){
+		PrintMsg("You are not allowed to print your label outside the drawing area, choose your place again...");
+		pWind->WaitMouseClick(r_GfxInfo.x1, r_GfxInfo.y1);
+	}
+	pWind->DrawString(r_GfxInfo.x1, r_GfxInfo.y1, lbl);
+}
+
+Output::~Output()
+{
+	delete pWind;
+}
